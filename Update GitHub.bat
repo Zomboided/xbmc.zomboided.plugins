a3634ae072354592b10e5d51a49b7abaefd6796d
Zomboided <mattedsmith@googlemail.com>
2016-12-15 17:48:50 +0000

    Update 2.2.5 again

---
+++ Update GitHub.bat
@@ -0,0 +1,10 @@
+SET GITPATH=c:\GitHub\
+
+REM Create updated zip files for all of the plugins\gen_addons
+
+
+REM Copy addon.xml for all of the plugins into the resepective plugin directories
+
+
+REM Update repository data
+python %GITPATH%\xbmc.zomboided.plugins\gen_addons.py
