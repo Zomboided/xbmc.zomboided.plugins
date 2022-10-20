782d39641351641451a4b8f8c7acf485e66bc17f
Zomboided <mattedsmith@googlemail.com>
2022-10-20 13:27:20 +0100

    Build changes

--- gen_addons.py
+++ gen_addons.py
@@ -2,7 +2,7 @@
 """ addons.xml generator """
 
 import os
-import md5
+import hashlib
 
 
 class Generator:
@@ -16,7 +16,7 @@ class Generator:
         self._generate_addons_file()
         self._generate_md5_file()
         # notify user
-        print "Finished updating addons xml and md5 files"
+        print ("Finished updating addons xml and md5 files")
 
     def _generate_addons_file( self ):
         # addon list
@@ -30,7 +30,7 @@ class Generator:
             try:
                 # skip any file or .git folder
                 if ( not os.path.isdir( addon ) or addon == ".git" ): continue
-                print addon
+                print (addon)
                 # create path
                 _path = os.path.join( addon, "addon.xml" )
                 # split lines for stripping
@@ -42,34 +42,34 @@ class Generator:
                     # skip encoding format line
                     if ( line.find( "<?xml" ) >= 0 ): continue
                     # add line
-                    addon_xml += unicode( line.rstrip() + "\n", "utf-8" )
+                    addon_xml += line.rstrip() + "\n"
                     # we succeeded so add to our final addons.xml text
                 addons_xml += addon_xml.rstrip() + "\n\n"
-            except Exception, e:
+            except Exception as e:
                 # missing or poorly formatted addon.xml
-                print "Excluding %s for %s" % ( _path, e, )
+                print ("Excluding %s for %s" % ( _path, e, ))
             # clean and add closing tag
         addons_xml = addons_xml.strip() + u"\n</addons>\n"
         # save file
-        self._save_file( addons_xml.encode( "utf-8" ), file="addons.xml" )
+        self._save_file( addons_xml, file="addons.xml" )
 
     def _generate_md5_file( self ):
         try:
             # create a new md5 hash
-            m = md5.new( open( "addons.xml" ).read() ).hexdigest()
+            m = hashlib.md5( open("addons.xml", 'rb').read() ).hexdigest()
             # save file
             self._save_file( m, file="addons.xml.md5" )
-        except Exception, e:
+        except Exception as e:
             # oops
-            print "An error occurred creating addons.xml.md5 file!\n%s" % ( e, )
+            print ("An error occurred creating addons.xml.md5 file!\n%s" % ( e, ))
 
     def _save_file( self, data, file ):
         try:
             # write data to the file
             open( file, "w" ).write( data )
-        except Exception, e:
+        except Exception as e:
             # oops
-            print "An error occurred saving %s file!\n%s" % ( file, e, )
+            print ("An error occurred saving %s file!\n%s" % ( file, e, ))
 
 
 if ( __name__ == "__main__" ):
